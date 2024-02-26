f16c3fdcc614d084f622951e5a6444c1ccdc9b25
Mahmoudbaky <ma7mouudbaky@gmail.com>
2024-02-27 00:59:22 +0200

    wrote INTx macros

--- MCAL/INTERRUPT/mcal_external_interrupt.h
+++ MCAL/INTERRUPT/mcal_external_interrupt.h
@@ -8,7 +8,72 @@
 #ifndef MCAL_EXTERNAL_INTERRUPT_H
 #define	MCAL_EXTERNAL_INTERRUPT_H
 
+/* INCLUDS */
+
 #include "mcal_interrupt_config.h"
+
+/* MACRO DECLARATIONS */
+
+/* MACRO FUNTIONS DECLARATIONS */
+
+#if INTERRUPT_INTx_FEATURE==INTERRUPT_ENABLE_FEATURE
+
+/* This routine clears the interrupt enable for the external interrupt, INT0 */
+#define EXT_INT0_InterruptDisable()         (INTCONbits.INT0IE = 0)
+/* This routine sets the interrupt enable for the external interrupt, INT0 */
+#define EXT_INT0_InterruptEnable()          (INTCONbits.INT0IE = 1)
+/* This routine clears the interrupt flag for the external interrupt, INT0 */
+#define EXT_INT0_InterruptFlagClear()       (INTCONbits.INT0IF = 0)
+/* This routine set the edge detect of the extern interrupt to negative edge */
+#define EXT_INT0_RisingEdgeSet()            (INTCON2bits.INTEDG0 = 1)
+/* This routine set the edge detect of the extern interrupt to positive edge */
+#define EXT_INT0_FallingEdgeSet()           (INTCON2bits.INTEDG0 = 0)
+
+/* This routine clears the interrupt enable for the external interrupt, INT1 */
+#define EXT_INT1_InterruptDisable()         (INTCON3bits.INT1IE = 0)
+/* This routine sets the interrupt enable for the external interrupt, INT1 */
+#define EXT_INT1_InterruptEnable()          (INTCON3bits.INT1IE = 1)
+/* This routine clears the interrupt flag for the external interrupt, INT1 */
+#define EXT_INT1_InterruptFlagClear()       (INTCON3bits.INT1IF = 0)
+/* This routine set the edge detect of the extern interrupt to negative edge */
+#define EXT_INT1_RisingEdgeSet()            (INTCON2bits.INTEDG1 = 1)
+/* This routine set the edge detect of the extern interrupt to positive edge */
+#define EXT_INT1_FallingEdgeSet()           (INTCON2bits.INTEDG1 = 0)
+
+/* This routine clears the interrupt enable for the external interrupt, INT2 */
+#define EXT_INT2_InterruptDisable()         (INTCON3bits.INT2IE = 0)
+/* This routine sets the interrupt enable for the external interrupt, INT2 */
+#define EXT_INT2_InterruptEnable()          (INTCON3bits.INT2IE = 1)
+/* This routine clears the interrupt flag for the external interrupt, INT2 */
+#define EXT_INT2_InterruptFlagClear()       (INTCON3bits.INT2IF = 0)
+/* This routine set the edge detect of the extern interrupt to negative edge */
+#define EXT_INT2_RisingEdgeSet()            (INTCON2bits.INTEDG2 = 1)
+/* This routine set the edge detect of the extern interrupt to positive edge */
+#define EXT_INT2_FallingEdgeSet()           (INTCON2bits.INTEDG2 = 0)
+
+#if INTERRUPT_PRIORITY_FEATURE==INTERRUPT_ENABLE_FEATURE
+
+/* This routine set the INT1 External Interrupt Priority to be High priority */
+#define EXT_INT1_HighPrioritySet()           (INTCON3bits.INT1IP = 1)
+/* This routine set the INT1 External Interrupt Priority to be Low priority */
+#define EXT_INT1_LowPrioritySet()            (INTCON3bits.INT1IP = 0)
+/* This routine set the INT2 External Interrupt Priority to be High priority */
+#define EXT_INT2_HighPrioritySet()           (INTCON3bits.INT2IP = 1)
+/* This routine set the INT2 External Interrupt Priority to be Low priority */
+#define EXT_INT2_LowPrioritySet()            (INTCON3bits.INT2IP = 0)
+
+#endif
+
+
+
+#endif
+
+
+/* DATA TYPE DECLARATIONS (ENUMS,UNIONS,STRUCTS) */
+
+
+/* FUNTIONS DECLARATIONS */
+
 
 #endif	/* MCAL_EXTERNAL_INTERRUPT_H */
 
