f8c3f65e843dc17ad31560cd42bf59aff9209166
Mahmoudbaky <ma7mouudbaky@gmail.com>
2024-02-27 01:02:29 +0200

    new file to enable and disable some functions

---
+++ MCAL/INTERRUPT/mcal_interrupt_gen_cfg.h
@@ -0,0 +1,20 @@
+/* 
+ * File:   mcal_interrupt_gen_cfg.h
+ * Author: houda
+ *
+ * Created on February 26, 2024, 7:56 PM
+ */
+
+#ifndef MCAL_INTERRUPT_GEN_CFG_H
+#define	MCAL_INTERRUPT_GEN_CFG_H
+
+#define INTERRUPT_ENABLE_FEATURE    1
+#define INTERRUPT_PRIORITY_FEATURE  INTERRUPT_ENABLE_FEATURE
+#define INTERRUPT_INTx_FEATURE      INTERRUPT_ENABLE_FEATURE
+#define INTERRUPT_ON_CHANGE_FEATURE INTERRUPT_ENABLE_FEATURE
+
+
+
+
+#endif	/* MCAL_INTERRUPT_GEN_CFG_H */
+
