9223d6ce28e5e98d81b9bc0c87f4c0a4ec43760e
Mahmoudbaky <ma7mouudbaky@gmail.com>
2024-03-22 09:59:58 +0200

    finishing the adc module ......

--- MCAL/ADC/mcal_adc.c
+++ MCAL/ADC/mcal_adc.c
@@ -7,3 +7,179 @@
 
 #include "mcal_adc.h"
 
+
+/* HELPER FUNCTIONS DECLERATIONS */
+static inline void adc_input_channel_port_configuration(const adc_config_t *adc);
+static inline void select_result_format(const adc_config_t *adc);
+static inline void configure_VREF(const adc_config_t *adc);
+
+
+/* API FUNCTIONS */
+Std_ReturnType ADC_Init(const adc_config_t *adc){
+    Std_ReturnType ret = E_OK;
+    if(NULL == adc){
+        ret = E_NOT_OK;
+    }
+    else{
+        /* disable the adc */
+        ADC_DISABLE();
+        /* configure the acquistions time */
+        ADCON2bits.ACQT = adc->adc_aquisition_time;
+        /* configure the conversion clock */
+        ADCON2bits.ADCS = adc->adc_conversion_clock;
+        /* configure the default channel */
+        ADCON0bits.CHS = adc->adc_Channel_select;
+        adc_input_channel_port_configuration(adc);
+        /* configure the interrupt */
+        
+        /* configure the result format */
+        select_result_format(adc);
+        /* configure the voltage reference */
+        configure_VREF(adc);
+        /* enable the adc */
+        ADC_ENABLE();
+    }
+    return ret;
+}
+
+Std_ReturnType ADC_DeInit(const adc_config_t *adc){
+    Std_ReturnType ret = E_OK;
+    if(NULL == adc){
+        ret = E_NOT_OK;
+    }
+    else{
+        /* disable the adc */
+        ADC_DISABLE();
+        /* configure the interrupt */
+        
+    }
+    return ret;
+}
+
+Std_ReturnType ADC_SelectChannel(const adc_config_t *adc, adc_Channel_select_t channel){
+    Std_ReturnType ret = E_OK;
+    if(NULL == adc){
+        ret = E_NOT_OK;
+    }
+    else{
+       
+    }
+    return ret;
+}
+
+Std_ReturnType ADC_StartConversion(const adc_config_t *adc){
+    Std_ReturnType ret = E_OK;
+    if(NULL == adc){
+        ret = E_NOT_OK;
+    }
+    else{
+       
+    }
+    return ret;
+}
+
+Std_ReturnType ADC_IsConversionDone(const adc_config_t *adc, uint8 *conversion_status){
+    Std_ReturnType ret = E_OK;
+    if((NULL == adc) || (NULL == conversion_status)){
+        ret = E_NOT_OK;
+    }
+    else{
+       
+    }
+    return ret;
+}
+
+Std_ReturnType ADC_GetResult(const adc_config_t *adc, adc_result_t *result){
+    Std_ReturnType ret = E_OK;
+    if((NULL == adc) || (NULL == result)){
+        ret = E_NOT_OK;
+    }
+    else{
+       
+    }
+    return ret;
+}
+
+Std_ReturnType ADC_GetConversion(const adc_config_t *adc
+                                ,adc_Channel_select_t channel
+                                ,adc_result_t *result){
+    Std_ReturnType ret = E_OK;
+    if((NULL == adc) || (NULL == result)){
+        ret = E_NOT_OK;
+    }
+    else{
+       
+    }
+    return ret;
+}
+
+/* HELPER FUNCTIONS */
+static inline void adc_input_channel_port_configuration(const adc_config_t *adc){
+    switch(adc->adc_Channel_select){
+        case ADC_CHANNEL_0:
+            SET_BIT(TRISA, _TRISA_RA0_POSN);
+            break;
+        case ADC_CHANNEL_1:
+            SET_BIT(TRISA, _TRISA_RA1_POSN);
+            break;
+        case ADC_CHANNEL_2:
+            SET_BIT(TRISA, _TRISA_RA2_POSN);
+            break;
+        case ADC_CHANNEL_3:
+            SET_BIT(TRISA, _TRISA_RA3_POSN);
+            break;
+        case ADC_CHANNEL_4:
+            SET_BIT(TRISA, _TRISA_RA5_POSN);
+            break;
+        case ADC_CHANNEL_5:
+            SET_BIT(TRISE, _TRISE_RE0_POSN);
+            break;
+        case ADC_CHANNEL_6:
+            SET_BIT(TRISE, _TRISE_RE1_POSN);
+            break;
+        case ADC_CHANNEL_7:
+            SET_BIT(TRISE, _TRISE_RE2_POSN);
+            break;
+        case ADC_CHANNEL_8:
+            SET_BIT(TRISB, _TRISB_RB2_POSN);
+            break;
+        case ADC_CHANNEL_9:
+            SET_BIT(TRISB, _TRISB_RB3_POSN);
+            break;
+        case ADC_CHANNEL_10:
+            SET_BIT(TRISB, _TRISB_RB1_POSN);
+            break;
+        case ADC_CHANNEL_11:
+            SET_BIT(TRISB, _TRISB_RB4_POSN);
+            break;
+        case ADC_CHANNEL_12:
+            SET_BIT(TRISB, _TRISB_RB0_POSN);
+            break;
+        default: /* NOTHING */
+            break;
+    }
+}
+
+static inline void select_result_format(const adc_config_t *adc){
+    if(ADC_RIGHT_JUSTIFIED_RESULT == adc->adc_result_format){
+        ADC_RIGHT_JUSTIFIED();
+    }
+    else if (ADC_LEFT_JUSTIFIED_RESULT == adc->adc_result_format){
+        ADC_LEFT_JUSTIFIED();
+    }
+    else{
+        /* NOTHING */
+    }
+}
+
+static inline void configure_VREF(const adc_config_t *adc){
+    if(ADC_VOLTAGE_REF_ENABLE == adc->adc_voltage_ref){
+        ADC_ENABLE_VOLTAGE_REF();
+    }
+    else if (ADC_VOLTAGE_REF_DISABLE == adc->adc_voltage_ref){
+        ADC_DISABLE_VOLTAGE_REF();
+    }
+    else{
+        /* NOTHING */
+    }
+}
--- MCAL/ADC/mcal_adc.h
+++ MCAL/ADC/mcal_adc.h
@@ -21,7 +21,7 @@
 /*
     * Description: ADC source selection
 */
-#define ADC_AN0_ANALOG_FUNCTIONALITY  0x0E
+#define ADC_AN0_ANALOG_FUNCTIONALITY  0x0E  
 #define ADC_AN1_ANALOG_FUNCTIONALITY  0x0D
 #define ADC_AN2_ANALOG_FUNCTIONALITY  0x0C
 #define ADC_AN3_ANALOG_FUNCTIONALITY  0x0B
@@ -34,17 +34,27 @@
 #define ADC_AN10_ANALOG_FUNCTIONALITY 0x04
 #define ADC_AN11_ANALOG_FUNCTIONALITY 0x03
 #define ADC_AN12_ANALOG_FUNCTIONALITY 0x02
-
 #define ALL_ANALOG_PINS  0x00
 #define ALL_DIGITAL_PINS 0x0F
+
+#define ADC_RIGHT_JUSTIFIED_RESULT  0x01U
+#define ADC_LEFT_JUSTIFIED_RESULT   0x00U
+
+#define ADC_VOLTAGE_REF_ENABLE  0x01U
+#define ADC_VOLTAGE_REF_DISABLE 0x00U
 
 /* MACRO FUNTIONS DECLARATIONS */
 
 /*
     * Description: ADC status 
-    * @return: 1 if the ADC is busy, 0 if the ADC is not busy
+    * @return: 1 if the ADC is busy, 0 A/D idle
 */
 #define ADC_CONVERSION_STATUS() (ADCON0bits.GO_nDONE)
+
+/*
+    * Description: macro to start the conversion
+*/
+#define ADC_START_CONVERSION()  (ADCON0bits.GO_nDONE = 1) 
 
 /*
     * Description: ADC enable
@@ -65,6 +75,12 @@
                                     ADCON1bits.VCFG0 = 0;\
                                     ADCON1bits.VCFG1 = 0;\
                                 }while(0)
+
+#define ADC_ANALOG_DIGITAL_FUNCTIONALITY(_CONFIG) (ADCON1bits.PCFG = _CONFIG)
+
+
+#define ADC_RIGHT_JUSTIFIED()   (ADCON2bits.ADFM = 1)
+#define ADC_LEFT_JUSTIFIED()    (ADCON2bits.ADFM = 0)
 
 
 /* DATA TYPE DECLARATIONS (ENUMS,UNIONS,STRUCTS) */
@@ -87,9 +103,51 @@ typedef enum {
     ADC_CHANNEL_10,
     ADC_CHANNEL_11,
     ADC_CHANNEL_12
-}ADC_Channel_select_t;
+}adc_Channel_select_t;
+
+
+typedef enum {
+    ADC_0_TAD = 0,
+    ADC_2_TAD,
+    ADC_4_TAD,
+    ADC_6_TAD,
+    ADC_8_TAD,
+    ADC_12_TAD,
+    ADC_16_TAD,
+    ADC_20_TAD
+}adc_aquisition_time_t;
+
+typedef enum {
+    ADC_FOSC_2 = 0,
+    ADC_FOSC_8,
+    ADC_FOSC_32,
+    ADC_FRC,
+    ADC_FOSC_4,
+    ADC_FOSC_16,
+    ADC_FOSC_64
+}adc_conversion_clock_t;
+
+typedef struct {
+    void (*adc_InterruptHandler)(void);                     /* callback function */
+    adc_aquisition_time_t  adc_aquisition_time;     /* @ref adc adc_aquisition_time_t */
+    adc_conversion_clock_t adc_conversion_clock;    /* @ref adc_conversion_clock_t */
+    adc_Channel_select_t   adc_Channel_select;      /* @ref adc_Channel_select_t */
+    uint8                adc_voltage_ref   : 1;   /* voltage ref configuration */
+    uint8                adc_result_format : 1;   /* result format */
+    uint8                adc_reserved      : 6;
+}adc_config_t;
+
+typedef uint16 adc_result_t;
 
 /* FUNTIONS DECLARATIONS */
+
+Std_ReturnType ADC_Init(const adc_config_t *adc);
+Std_ReturnType ADC_DeInit(const adc_config_t *adc);
+Std_ReturnType ADC_SelectChannel(const adc_config_t *adc, adc_Channel_select_t channel);
+Std_ReturnType ADC_StartConversion(const adc_config_t *adc);
+Std_ReturnType ADC_IsConversionDone(const adc_config_t *adc, uint8 *conversion_status);
+Std_ReturnType ADC_GetResult(const adc_config_t *adc, adc_result_t *result);
+Std_ReturnType ADC_GetConversion(const adc_config_t *adc, adc_Channel_select_t channel , adc_result_t *result);
 
 
 #endif	/* MCAL_ADC_H */
